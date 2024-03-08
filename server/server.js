31ac5351db7282fd161ffbddb91edfa62ea1560e
razvaniordan <iordanalexandru21@stud.ase.ro>
2024-03-09 01:13:46 +0200

    Commit #3
    -added the frontend for the platform main page
    -added the functionality for Copy and Log out buttons
    -added the erase of the username and password inputs after pressing log in if they are wrong
    
    +Note: add JWT token (more secure and debug the login/logout problem)

--- server/server.js
+++ server/server.js
@@ -10,15 +10,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../frontend/Login')));
+app.use(express.static(path.join(__dirname, '../frontend/Platform')));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('../frontend/Login'));
+app.use(express.static('../frontend/Platform'));
+
 
 // This and the app.use(express.static()) I use in order to change the path of the html and connect frontend to the backend of the login
 // and the app would run on localhost:3000 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/Login/Login.html'));
 });
+
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
